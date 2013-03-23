3b0c663cabd2bfda5a498b0b1ea30a171f5b1417
JP Richardson <jprichardson@gmail.com>
2013-03-23 00:15:16 -0500

    added getTemplateDir() to SkyEnv

--- test/lib/sky_env.test.js
+++ test/lib/sky_env.test.js
@@ -95,7 +95,6 @@ describe('SkyEnv', function() {
     describe('> when specified in config', function() {
       it('it should return the proper template', function() {
         process.chdir(TEST_DIR)
-        var articlesDir = path.join(TEST_DIR, 'articles')
         fs.writeJsonSync(CFG_FILE, {blog: {template: 'shiny'}})
         
         var se = new SkyEnv(libsky.findBaseDirSync())
@@ -107,13 +106,22 @@ describe('SkyEnv', function() {
     describe('> when its not specified in config', function() {
       it('it should return the proper template', function() {
         process.chdir(TEST_DIR)
-        var articlesDir = path.join(TEST_DIR, 'articles')
         fs.writeJsonSync(CFG_FILE, {})
         
         var se = new SkyEnv(libsky.findBaseDirSync())
         se.loadConfigsSync()
         EQ (se.getTemplate(), 'basic')
       })
+    })
+  })
+
+  describe('- getTemplate()', function() {
+    it('should retrieve the template dir', function() {
+      process.chdir(TEST_DIR)
+        
+      var se = new SkyEnv(libsky.findBaseDirSync())
+      se.loadConfigsSync()
+      EQ (removePrivate(se.getTemplateDir()), path.join(TEST_DIR, 'sky', 'templates', se.getTemplate()))
     })
   })
 })
